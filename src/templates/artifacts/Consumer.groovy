05610a80a23c0fee8c5b7eb241315a541e5a8df9
Aaron Brown <brown.aaron.lloyd@gmail.com>
2013-10-22 20:15:58 -0500

    CreateConsumer Script.
    Script to create the template of a
    grails-application/rabbit-consumers/*Consumer.groovy Class.

---
+++ src/templates/artifacts/Consumer.groovy
@@ -0,0 +1,20 @@
+@artifact.package@
+class @artifact.name@ {
+
+    static rabbitConfig = [
+        // TODO: Setup config.
+    ]
+
+    /**
+     * Handle the incoming Rabbit message.
+     *
+     * @param body      The body as a Byte-array.
+     *
+     * @param context   Additional information available from the message.
+     *
+     * @return
+     */
+    def handleMessage(def body, MessageContext context) {
+        // TODO: Handle Message.
+    }
+}
