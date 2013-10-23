819baf2759dd681e40b972d4bc2ea26e2b2b2e83
Aaron Brown <brown.aaron.lloyd@gmail.com>
2013-10-22 21:28:25 -0500

    Create multiple Consumers in one call.
    grails create-consumer my.package.AlphaConsumer my.package.Beta
    
    yields:
    
    grails-app/rabbit-consumers/my/package/AlphaConsumer.groovy
    grails-app/rabbit-consumers/my/package/BetaConsumer.groovy

--- scripts/CreateConsumer.groovy
+++ scripts/CreateConsumer.groovy
@@ -27,8 +27,10 @@ target(createConsumer: "Creates a new Rabbit-native Consumer class.") {
     def type = "Consumer"
     promptForName(type: type)
 
-    def name = argsMap["params"][0]
-    createArtifact(name: name, suffix: type, type: type, path: "grails-app/rabbit-consumers")
+    for (name in argsMap["params"]) {
+        name = purgeRedundantArtifactSuffix(name, type)
+        createArtifact(name: name, suffix: type, type: type, path: "grails-app/rabbit-consumers")
+    }
 }
 
 setDefaultTarget createConsumer
