74cc1d6106ae92b7e20da88fbfe3b69542299f8b
Bud Byrd <bud.byrd@rackspace.com>
2014-04-03 10:26:07 -0500

    Internal 2.0.0 release.

--- RabbitmqNativeGrailsPlugin.groovy
+++ RabbitmqNativeGrailsPlugin.groovy
@@ -33,7 +33,7 @@ class RabbitmqNativeGrailsPlugin {
     /**
      * Version of the plugin.
      */
-    def version = "1.0.3"
+    def version = "2.0.0"
 
     /**
      * The version or versions of Grails the plugin is designed for.
