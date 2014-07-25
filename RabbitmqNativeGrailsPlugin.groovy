cc61eb7dfffd53cfa51682410b0755fe518d1ea8
Bud Byrd <bud.byrd@rackspace.com>
2014-07-25 14:17:53 -0500

    Fix formatting issue with quickstart, and updated connection configuration.

--- RabbitmqNativeGrailsPlugin.groovy
+++ RabbitmqNativeGrailsPlugin.groovy
@@ -33,7 +33,7 @@ class RabbitmqNativeGrailsPlugin {
     /**
      * Version of the plugin.
      */
-    def version = "2.0.2"
+    def version = "2.0.3"
 
     /**
      * The version or versions of Grails the plugin is designed for.
