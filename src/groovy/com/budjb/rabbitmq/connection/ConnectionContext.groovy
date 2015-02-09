71be1d5fbdff4580980b6903f14ac8ce9319c757
Bud Byrd <bud.byrd@rackspace.com>
2015-02-09 13:26:11 -0600

    Add/update licences.

--- src/groovy/com/budjb/rabbitmq/connection/ConnectionContext.groovy
+++ src/groovy/com/budjb/rabbitmq/connection/ConnectionContext.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Bud Byrd
+ * Copyright 2014-2015 Bud Byrd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
